0add8f84de308d461910862bdf77c4f7a8f01a1e
oscar000803 <oscar000803@gmail.com>
2021-06-30 16:20:04 +0800

    revise docker-compose file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import dotenv from 'dotenv-defaults';
 dotenv.config();
 
-const instance = axios.create({ baseURL: "http://34.81.171.221:5000" })
+const instance = axios.create({ baseURL: process.env.REACT_APP_SERVER_URL })
 
 const sendMessage = async (type, url, message) => {
     console.log("SEND MESSAGE:", message)
